40a9b15e51911b1807005d3865123197814ae8e5
Nick Karastamatis <nkarastamatis@gmail.com>
2013-09-29 22:43:28 -0400

    c

--- GettingStartedWithCSS3/js/lesson.js
+++ GettingStartedWithCSS3/js/lesson.js
@@ -1,5 +1,6 @@
 ﻿var slides;
 var currentSlide;
+var currentNavBarID;
 var slideDisplay;
 
  function initLesson (element){
@@ -7,19 +8,26 @@ var slideDisplay;
     elem.addEventListener('click', showSlide);
     slides = element.getElementsByClassName('slide');
     slideDisplay = document.getElementById('slideDisplay');
-}
+    currentNavBarID = null;
+ }
 
     function showSlide(e) {
-        if (e.target.nodeName === 'H3') {
+        if (e.target.nodeName === 'DIV') {
             if (e.target.getAttribute('data-demopage')) {
                 showDemo(e);
                 return;
             }
 
+            if (currentNavBarID)
+                document.getElementById(currentNavBarID).style.borderColor = 'transparent';
+
+            currentNavBarID = e.target.id;
+            e.target.style.border = 'solid';
+
             if (currentSlide)
                 currentSlide.style.visibility = 'hidden';
 
-            currentSlide = slides[e.target.id];
+            currentSlide = slides[currentNavBarID];
             currentSlide.style.visibility = 'visible';
 
             slideDisplay.innerHTML = currentSlide.innerHTML;
